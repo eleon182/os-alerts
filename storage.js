d4e9abd437df396d159975ee746409f1c7b115b3
Esteban Leon <eleon182@gmail.com>
2016-08-01 16:13:31 -0700

    wire up s3

--- storage.js
+++ storage.js
@@ -1,5 +1,6 @@
 var lo = require('lodash');
 var fs = require('fs');
+var s3 = require('./s3.js');
 
 module.exports = {
     init: init,
@@ -16,28 +17,35 @@ function store(data) {
 }
 
 function get() {
-    if(options.storageRoute === 's3'){
+    if (options.storageRoute === 's3') {
         return getS3();
-    }
-    else if(options.storageRoute === 'local'){
+    } else if (options.storageRoute === 'local') {
         return getLocal();
     }
 }
 
 function init(options) {
+    if (options.s3) {
+        s3.init({
+            bucket: 'log'
+        });
+        options.storageRoute = 's3';
+    } else {
+        options.storageRoute = 'local';
+    }
     return true;
 }
 
-function getS3() {
-
+function getS3(data, callback) {
+    s3.getObject(data, callback);
 }
 
 function getLocal() {
 
 }
 
-function storeS3(data) {
-
+function storeS3(data, callback) {
+    s3.addObject(data, callback);
 }
 
 function storeLocal(data) {
