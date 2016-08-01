1add5d29c204764c72bfb67fd840ef47032d1845
Esteban Leon <eleon182@gmail.com>
2016-08-01 09:40:37 -0700

    build out storage options

--- storage.js
+++ storage.js
@@ -2,17 +2,44 @@ var lo = require('lodash');
 var fs = require('fs');
 
 module.exports = {
-    init: init
+    init: init,
+    store: store,
+    get: get
 };
+
+var options = {
+    storageRoute: null
+};
+
+function store(data) {
+
+}
+
+function get() {
+    if(options.storageRoute === 's3'){
+        return getS3();
+    }
+    else if(options.storageRoute === 'local'){
+        return getLocal();
+    }
+}
 
 function init(options) {
     return true;
 }
 
-function storeS3(data){
+function getS3() {
 
 }
 
-function storeLocal(data){
+function getLocal() {
+
+}
+
+function storeS3(data) {
+
+}
+
+function storeLocal(data) {
 
 }
